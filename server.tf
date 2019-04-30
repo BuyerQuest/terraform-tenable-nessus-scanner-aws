467d729d07ec01cfd91781e6d5279b209d77f317
Bradley Boveinis <bradley.boveinis@sage.com>
2019-04-30 12:29:48 +1000

    change owners id to friendly name

--- server.tf
+++ server.tf
@@ -14,7 +14,7 @@ data "template_file" "nessus-user-data" {
 # Find the latest AMI by product code
 data "aws_ami" "nessus-image" {
   most_recent = true
-  owners = ["679593333241"]
+  owners = ["aws-marketplace"]
 
   filter {
     name   = "product-code"

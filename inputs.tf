8de923610e86bb4dee956847913439e932c3f877
Andrew Bobulsky <andrew.bobulsky@buyerquest.com>
2020-04-09 12:09:06 -0400

    Fix local.instance_tags. I broke it with the last commit.

--- inputs.tf
+++ inputs.tf
@@ -48,10 +48,10 @@ variable "use_eip" {
 ## Process some inputs into a map of tags, then use those instead
 
 locals {
-  instance_tags = { "Name" = coalesce(
-    var.instance_name,                       # Override with explicit name input
+  instance_name = coalesce(
+    var.instance_name,                       # Prefer explicit name input
     lookup(var.instance_tags, "Name", null), # Allow naming with tags
     "nessus-scanner"                         # Default instance name
-    )
-  }
+  )
+  instance_tags = merge(var.instance_tags, { "Name" = local.instance_name }) # The right-most map's value always wins
 }

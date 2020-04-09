c62a69864909ab2c9c10ab4ada8426f4205daa47
Andrew Bobulsky <andrew.bobulsky@buyerquest.com>
2020-04-09 13:18:11 -0400

    Feature/remove template (#8)
    * Use coalesce instead of merge+ternary
    
    This is mostly my opinion, but it results in cleaner code. Resolves #5
    
    * Fix local.instance_tags. I broke it with the last commit.
    
    * Remove user_data template
    
    Switch to direct jsonencode() of relevant values inside of a map

--- server.tf
+++ server.tf
@@ -1,10 +1,8 @@
 ## Nessus VM Scanner Server Provision ##
 
 # Generate cloud-init
-data "template_file" "nessus-user-data" {
-  template = file("${path.module}/templates/user-data.json.tpl")
-
-  vars = {
+locals {
+  user_data_map = {
     name     = coalesce(var.scanner_name, local.instance_tags["Name"])
     key      = var.tenable_linking_key
     iam_role = aws_iam_role.nessus-server-role.name
@@ -28,7 +26,7 @@ resource "aws_instance" "nessus-scanner" {
   vpc_security_group_ids = [aws_security_group.nessus-security-group.id]
   iam_instance_profile   = aws_iam_instance_profile.nessus-server-profile.name
   subnet_id              = var.subnet_id
-  user_data              = data.template_file.nessus-user-data.rendered
+  user_data              = jsonencode(local.user_data_map)
   instance_type          = var.instance_type
 
   tags = local.instance_tags
